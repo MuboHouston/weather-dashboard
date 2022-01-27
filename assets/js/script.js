53eb37edbe36ba8fdb65150a05a6871dd74bcf2c
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-01-27 15:44:47 -0600

    fetch current and future weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,38 +12,62 @@ var formSubmitHandler = function(event) {
     var cityName = cityInputEl.value.trim();
 
     if(cityName) {
-        getWeather(cityName);
+        getCurrentWeather(cityName);
+        getFutureWeather(cityName);
+        getCurrentDate();
         cityInputEl.value = "";
     } else {
         alert("Please enter city name")
     }
 }
 
-var getWeather = function(cityName) {
+var getCurrentWeather = function(cityName) {
     //format the weather api url
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=9e1b9c65c5a45c7606cbddd777f0e91b";
     
     //make a request to the url
-    fetch(apiUrl).then(function(response) {
-    response.json().then(function(data) {
-        displayWeather(data, cityName)
-    })
+    fetch(apiUrl)
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+            // displayWeather(data, cityName);
+            console.log(data);
+        });
+    }else{
+        alert("Please enter valid city name");
+    }
 })
 }
 
-var displayWeather = function(current, searchTerm){
+var getFutureWeather = function(cityName) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&cnt=5&units=imperial&&appid=37b206da464f2ac783efca5a706e09d3";
+
+    fetch(apiUrl)
+    .then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+            });
+        }
+    })
+}
+
+var getCurrentDate = function(cityName) {
     var options = {month: "long", day: "numeric"};
     var today = new Date();
     var currentDay= today.toLocaleDateString("en-US", options);
+    dateEl.textContent = currentDay;
+}
 
+var displayWeather = function(current, searchTerm){
     currentWeatherContainerEl.textContent = "";
     citySearchTerm.textContent = searchTerm;
     //display the current time
-    dateEl.textContent = currentDay;
 
-    var temp = main;
-    var wind =;
-    var humidity =;
+    var temp = main.temp;
+    console.log(temp);
+    // var wind =;
+    // var humidity =;
 }
 
 searchFormEl.addEventListener("submit", formSubmitHandler);
